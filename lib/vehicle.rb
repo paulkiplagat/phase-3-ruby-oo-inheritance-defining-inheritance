12f3a05fd08eb33de5aacf9b05bb95afdd85e4ef
Paul <paul.kiplagt@student.moringaschool.com>
2022-08-18 23:20:41 +0300

    commit tree 5375af0078de64c8d502048fdd8d933495b8bb06

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,14 +1,17 @@
-class Vehicleattr_accessor :wheel_size, :wheel_number
-
-    def initialize(wheel_size, wheel_number)
-      @wheel_size = wheel_size
-      @wheel_number = wheel_number
-    end
+class Vehicle
     attr_accessor :wheel_size, :wheel_number
-
+  
     def initialize(wheel_size, wheel_number)
       @wheel_size = wheel_size
       @wheel_number = wheel_number
     end
   
-end
+    def go
+      "vrrrrrrrooom!"
+    end
+  
+    def fill_up_tank
+      "filling up!"
+    end
+  
+  end
