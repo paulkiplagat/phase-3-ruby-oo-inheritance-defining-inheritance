d45b56ebfe6f12db3d6153e5308fe5e897115664
Paul <paul.kiplagt@student.moringaschool.com>
2022-08-18 23:15:42 +0300

    done

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -4,7 +4,11 @@ class Vehicleattr_accessor :wheel_size, :wheel_number
       @wheel_size = wheel_size
       @wheel_number = wheel_number
     end
+    attr_accessor :wheel_size, :wheel_number
+
+    def initialize(wheel_size, wheel_number)
+      @wheel_size = wheel_size
+      @wheel_number = wheel_number
+    end
   
-
-
 end
